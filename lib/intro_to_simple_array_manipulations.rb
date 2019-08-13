3e3d5bb4f69381acc9e1abac016a18e65052a87d
Ting Ting Ye <tingtingye24@gmail.com>
2019-08-13 18:52:04 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -27,5 +27,22 @@ def using_concat(array1,array2)
 end
 
 def using_insert (array, element)
-  array.insert(element)
+  array.insert(4,element)
+end
+
+def using_uniq(array)
+  array.uniq
+end
+
+def using_flatten(array)
+  array.flatten 
+end
+
+def using_delete(array, string)\
+  array.delete(string) 
+end 
+
+def using_delete_at(array, integer)
+  array.delete(integer)
+end
   
