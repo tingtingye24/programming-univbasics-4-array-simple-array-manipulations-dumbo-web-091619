6542b64d812cc94f28236b9b24268f9e3a6f528a
Ting Ting Ye <tingtingye24@gmail.com>
2019-08-13 18:46:54 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,30 @@
+def using_push (array, string)
+  array.push(string)
+end
+
+def using_unshift(array, string)
+  array.unshift(string)
+end
+
+def using_pop(array)
+  array.pop 
+end
+
+def pop_with_args(array)
+  array.pop.pop
+end
+
+def using_shift(array)
+  array.shift 
+end
+
+def shift_with_args(array)
+  array.shift.shift
+end
+
+def using_concat(array1,array2)
+  array1 +array2
+end
+
+
+  
